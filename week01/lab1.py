ba91b623ffbe0eb4d8136af246af5f4d42ea79a4
pallabok <carrie.arevalo@georgebrown.ca>
2026-01-18 22:47:02 -0500

    lab 1 completed

---
+++ week01/lab1.py
@@ -0,0 +1,24 @@
+﻿# Sample Coding Questions 01 Week 01
+# Carrie Arevalo
+
+# Question 2: Defining Variables
+my_array = [1, 4, 7, 9]
+
+# Question 3: Order of operations
+a = 1
+b = 2
+c = 3
+d = 4
+
+e = (a - ((b ** c) // d)) + (a % c)
+
+# Question 4: Formatting
+temperature = 32.6
+msg = "The temperature today is {:.3f} degrees celsius"
+print(msg.format(temperature))
+
+# Question 5: Common Functions
+userAge = int(input("Please enter your age: "))
+
+userAge = userAge + 22
+print("Now showing the shop items filtered by age", userAge)
